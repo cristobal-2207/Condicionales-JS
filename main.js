c8c8129192d6c33fbf983bb576a9384c977c17be
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-10-17 11:08:01 -0300

    5to Commit: Se agrega funcionalidad a la sección Clave Secreta.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Constantes de las Secciones
 // Constantes de Agregando y/o quitando bordes
 const imagenBordes = document.querySelector("#imagenBordes");
 
@@ -5,14 +6,23 @@ const imagenBordes = document.querySelector("#imagenBordes");
 const autoSticker = document.querySelector("#stickerAuto");
 const gatitoSticker = document.querySelector("#stickerGatito");
 const corazonSticker = document.querySelector("#stickerCorazon");
-
 const verificarSticker = document.querySelector("#verificarSticker");
 const mensajeSticker = document.querySelector("#mensajeSticker");
 
+//Constantes de Clave Secreta 
+const primerDigito = document.querySelector("#primerDigito");
+const segundoDigito = document.querySelector("#segundoDigito");
+const tercerDigito = document.querySelector("#tercerDigito");
+const validarClave = document.querySelector("#validarClave");
+const mensajeClaveSecreta = document.querySelector("#mensajeClaveSereta");
+
+
+//Condiciones de las Secciones
 //Condiciones de Agregando y/o quitando bordes
 imagenBordes.addEventListener("click", () =>{
-  imagenBordes.classList.toggle("border")
+  imagenBordes.classList.toggle("bordes")
 })
+
 //Condiciones de Pidiendo un máximo de 10 stickers
 verificarSticker.addEventListener("click", () => {
   const sumatoria = parseInt(stickerAuto.value) + parseInt(stickerGatito.value) + parseInt(stickerCorazon.value);
@@ -20,5 +30,16 @@ verificarSticker.addEventListener("click", () => {
     mensajeSticker.innerHTML = `No seas barsa, llevas ${sumatoria} stickers <br> El maximo es 10 po!!`
   } else{
     mensajeSticker.textContent = `Llevas ${sumatoria} stickers.`
+  }
+})
+
+//Condiciones de Clave Secreta
+validarClave.addEventListener("click", () => {
+  if(parseInt(primerDigito.value) === 9 && parseInt(segundoDigito.value) === 1 &&parseInt(tercerDigito.value) === 1){
+    mensajeClaveSecreta.textContent = "Password 1 Correcto";
+  } else if(parseInt(primerDigito.value) === 7 && parseInt(segundoDigito.value) === 1 && parseInt(tercerDigito.value) === 4){
+    mensajeClaveSecreta.textContent = "Password 2 Correcto";
+  } else {
+    mensajeClaveSecreta.textContent = "Password Incorrecto";
   }
 })
