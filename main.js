0f3f5392a3bf1ba5b2525415ab7592bb91ff4d1b
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-10-17 09:34:25 -0300

    3er Commit: Se agrega funcionalidad a sección Agregar y/o quitar bordes.

---
+++ main.js
@@ -0,0 +1,5 @@
+//Agregando y/o quitando bordes
+const imagenBordes = document.querySelector("#imagenBordes");
+imagenBordes.addEventListener("click", () =>{
+  imagenBordes.classList.toggle("border")
+})
