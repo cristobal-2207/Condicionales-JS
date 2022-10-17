6c6a4b009ac0849132010ac304be8763fe25f1e7
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-10-17 10:10:51 -0300

    4to Commit: Se agrega funcionalidad a la sección de Pidiendo un hasta 10 Stickers.

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-//Agregando y/o quitando bordes
+// Constantes de Agregando y/o quitando bordes
 const imagenBordes = document.querySelector("#imagenBordes");
+
+//Constantes de Pidiendo un máximo de 10 stickers
+const autoSticker = document.querySelector("#stickerAuto");
+const gatitoSticker = document.querySelector("#stickerGatito");
+const corazonSticker = document.querySelector("#stickerCorazon");
+
+const verificarSticker = document.querySelector("#verificarSticker");
+const mensajeSticker = document.querySelector("#mensajeSticker");
+
+//Condiciones de Agregando y/o quitando bordes
 imagenBordes.addEventListener("click", () =>{
   imagenBordes.classList.toggle("border")
+})
+//Condiciones de Pidiendo un máximo de 10 stickers
+verificarSticker.addEventListener("click", () => {
+  const sumatoria = parseInt(stickerAuto.value) + parseInt(stickerGatito.value) + parseInt(stickerCorazon.value);
+  if(sumatoria > 10) {
+    mensajeSticker.innerHTML = `No seas barsa, llevas ${sumatoria} stickers <br> El maximo es 10 po!!`
+  } else{
+    mensajeSticker.textContent = `Llevas ${sumatoria} stickers.`
+  }
 })
